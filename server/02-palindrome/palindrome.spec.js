3f1ccbb61a68c217b83d76e36d4d9f4e4da5f1bd
Marty Rehder <martygeek@gmail.com>
2017-08-11 15:40:23 -0500

    full palindrome tests

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,46 +1,44 @@
+/* eslint-disable no-trailing-spaces */
 describe('the palindrome canary spec', () => {
   it('shows the test infrastructure works', () => {
     true.should.be.true();
   });
-//
-//   function isPalindrome(phrase) {
-//     if (phrase.trim().length === 0) return false;
-//     return phrase === phrase.split('').reverse().join('');
-//   }
-//
-  let isPalindrome = (word) => {
-    return true;
-  };
+
+  function isPalindrome(word) {
+    if (word.trim().length < 1) {
+      return false;
+    } else {
+      return word === word.split('').reverse().join('');
+    }
+  }
 
   describe('a palindrome should be', () => {
     it('true for mom', () => {
       isPalindrome('mom').should.be.true();
     });
-    it('false for dude');
-    it('false for ""');
-    it('true for a');
-    it('false for he eh');
-    it('false for home sweet home');
-    it('false for "     "');
+
+    it('false for dude', () => {
+      isPalindrome('dude').should.be.false();
+    });
+
+    it('false for ""', () => {
+      isPalindrome('').should.be.false();
+    });
+
+    it('true for a', () => {
+      isPalindrome('a').should.be.true();
+    });
+
+    it('true for he eh', () => {
+      isPalindrome('he eh').should.be.true();
+    });
+
+    it('false for home sweet home', () => {
+      isPalindrome('home sweet home').should.be.false();
+    });
+
+    it('false for "     "', () => {
+      isPalindrome('"    "').should.be.true();
+    });
   });
-//
-//     it('is not racecars', () => {
-//       isPalindrome('racecars').should.be.false();
-//     });
-//
-//     it('is not race car', () => {
-//       isPalindrome('race car').should.be.false();
-//     });
-//
-//     it('"   " is not a palindrome   ', () => {
-//       isPalindrome('   ').should.be.false();
-//     });
-//
-//     it('"" is not a palindrome', () => {
-//       isPalindrome('').should.be.false();
-//     });
-//
-//     it('is "mom dad mom"', () => {
-//       isPalindrome('mom dad mom').should.be.true();
-//     });
 });
