c33e919a7133180649de07363aa5e5d132bd8194
Marty Rehder <martygeek@gmail.com>
2017-08-11 14:55:49 -0500

    palidrome initial tests

--- server/00-canary/canary.spec.js
+++ server/00-canary/canary.spec.js
@@ -4,16 +4,24 @@ describe('the canary spec', () => {
   });
 
   let fahrenheit2celsius = (fahrenheit) => (fahrenheit - 32) * 5 / 9;
+  let celsius2fahrenheit = (celsius) => (celsius * 1.8) + 32;
   describe('fahrenheit2celsius', () => {
     it('fahrenheit2celsius returns 0 given 32', () => {
       fahrenheit2celsius(32).should.equal(0);
     });
+
     it('fahrenheit2celsius returns 10 given 50', () => {
-          fahrenheit2celsius(50).should.equal(10);
-        });
+      fahrenheit2celsius(50).should.equal(10);
+    });
+
     it('fahrenheit2celsius returns 100 given 212', () => {
-             fahrenheit2celsius(212).should.equal(100);
-           });
+      fahrenheit2celsius(212).should.equal(100);
+    });
+  });
+  describe('celsius2fahrenheit', () => {
+    it('celsius2fahrenheit returns 32 given 0', () => {
+      celsius2fahrenheit();
+    });
   });
 });
 
--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -2,35 +2,39 @@ describe('the palindrome canary spec', () => {
   it('shows the test infrastructure works', () => {
     true.should.be.true();
   });
-
-  function isPalindrome(phrase) {
-    if (phrase.trim().length === 0) return false;
-    return phrase === phrase.split('').reverse().join('');
-  }
-
-  describe('a palindrome', () => {
-    it('is racecar', () => {
-      isPalindrome('racecar').should.be.true();
-    });
-
-    it('is not racecars', () => {
-      isPalindrome('racecars').should.be.false();
-    });
-
-    it('is not race car', () => {
-      isPalindrome('race car').should.be.false();
-    });
-
-    it('"   " is not a palindrome   ', () => {
-      isPalindrome('   ').should.be.false();
-    });
-
-    it('"" is not a palindrome', () => {
-      isPalindrome('').should.be.false();
-    });
-
-    it('is "mom dad mom"', () => {
-      isPalindrome('mom dad mom').should.be.true();
-    });
+//
+//   function isPalindrome(phrase) {
+//     if (phrase.trim().length === 0) return false;
+//     return phrase === phrase.split('').reverse().join('');
+//   }
+//
+  describe('a palindrome should be', () => {
+    it('true for mom');
+    it('false for dude');
+    it('false for ""');
+    it('true for a');
+    it('false for he eh');
+    it('false for home sweet home');
+    it('false for "     "');
   });
+//
+//     it('is not racecars', () => {
+//       isPalindrome('racecars').should.be.false();
+//     });
+//
+//     it('is not race car', () => {
+//       isPalindrome('race car').should.be.false();
+//     });
+//
+//     it('"   " is not a palindrome   ', () => {
+//       isPalindrome('   ').should.be.false();
+//     });
+//
+//     it('"" is not a palindrome', () => {
+//       isPalindrome('').should.be.false();
+//     });
+//
+//     it('is "mom dad mom"', () => {
+//       isPalindrome('mom dad mom').should.be.true();
+//     });
 });
